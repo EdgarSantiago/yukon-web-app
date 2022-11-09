0f5eff300fe719acd6798cc78b7517d3ec4ef8ea
Chediak <shammcity00@gmail.com>
2022-11-09 11:30:45 -0300

    fix: font size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -72,7 +72,7 @@ export default function Navbar() {
         >
           <Link href="/">
             <Box>
-              <Heading fontFamily={"spaceage"}>YUKON</Heading>
+              <Heading fontFamily={"spaceage"} fontSize={"xxx-large"} >YUKON</Heading>
             </Box>
           </Link>
           <Show above="1000px">

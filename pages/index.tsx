900fac28382206b080f131ffd8271630c7067be5
EdgarSantiago <edgargsp777@gmail.com>
2022-11-08 22:23:07 -0300

    trangle

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,14 @@ import { Box, Heading } from "@chakra-ui/react";
 import Head from "next/head";
 import Image from "next/image";
 import Layout from "../components/layouts/layout";
+import TriangleComponent from "../components/Triangle";
 
 export default function Home() {
   return (
     <Layout title="Home">
-      <Box
-        backgroundImage="url('https://cdn.discordapp.com/attachments/1039562578340347969/1039687626355130389/image.png')"
-        backgroundPosition="center"
-        backgroundRepeat="no-repeat"
-        backgroundSize="100% 100%"
-        h="100vh"
-        textAlign={"center"}
-      ></Box>
+      <Box h="100vh" textAlign={"center"}>
+        <TriangleComponent />
+      </Box>
     </Layout>
   );
 }
